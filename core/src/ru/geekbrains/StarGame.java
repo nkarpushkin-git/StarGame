28966d8251a1ff2bb7a38e704e88229a90163bb5
nkarpushkin <karpushkin.na@gmail.com>
2020-02-17 21:09:52 +0900

    Changed background image

--- core/src/ru/geekbrains/StarGame.java
+++ core/src/ru/geekbrains/StarGame.java
@@ -5,15 +5,18 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	TextureRegion region;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("flower_bgnd.jpg");
+		region = new TextureRegion(img, 20, 25, 100, 50);
 	}
 
 	@Override
@@ -21,7 +24,8 @@ public class StarGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 0, 0, 512, 512);
+		batch.draw(region, 400, 400);
 		batch.end();
 	}
 	
