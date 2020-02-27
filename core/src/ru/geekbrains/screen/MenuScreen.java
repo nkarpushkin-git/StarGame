1e4a3e4b4eff03b07c3185c3b9529c522ae4a49e
nkarpushkin <karpushkin.na@gmail.com>
2020-02-27 19:51:42 +0900

    Lesson_3 (GeekBrains teachers code)

---
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -0,0 +1,57 @@
+package ru.geekbrains.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.geekbrains.base.BaseScreen;
+import ru.geekbrains.math.Rect;
+import ru.geekbrains.sprite.Background;
+
+public class MenuScreen extends BaseScreen {
+
+    private Texture img;
+    private Texture bg;
+    private Vector2 pos;
+
+    private Background background;
+
+    @Override
+    public void show() {
+        super.show();
+        img = new Texture("badlogic.jpg");
+        bg = new Texture("textures/bg.png");
+        pos = new Vector2();
+        background = new Background(bg);
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        Gdx.gl.glClearColor(0.5f, 0.9f, 0.4f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        background.draw(batch);
+        batch.draw(img, pos.x, pos.y, 0.5f, 0.5f);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        img.dispose();
+        bg.dispose();
+        super.dispose();
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        return super.touchDown(touch, pointer, button);
+    }
+}
